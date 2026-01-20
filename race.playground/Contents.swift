ebe6b4f6a1edf5ad86ce80d5fed588ca40ba69fb
sachinkumar <sachinmehtab@gmail.com>
2026-01-21 00:19:10 +0530

    ac

--- race.playground/Contents.swift
+++ race.playground/Contents.swift
@@ -1,3 +1,23 @@
-import UIKit
+import Foundation
+class ContentViewModel {
 
-var greeting = "Hello, playground"
+    // Define a global variable
+    var sharedValue: Int = 0
+
+    func operation() {
+
+        // Create a concurrent queue
+//        let concurrentQueue = DispatchQueue(label: "codecat15", attributes: .concurrent)
+        // concurrent cause racr
+        let concurrentQueue = DispatchQueue(label: "codecat15")
+        
+        // Perform concurrent reads and writes to the shared variable
+        concurrentQueue.async {
+            self.sharedValue = 42
+        }
+
+        concurrentQueue.async {
+            print(self.sharedValue) // Reading the shared variable
+        }
+    }
+}
